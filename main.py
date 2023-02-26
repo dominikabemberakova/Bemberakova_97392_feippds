29843fcff7a5766d16d5bd2c99cd4d6e029206c6
Dominika Bemberáková <dominikabemberakova@Dominikas-MacBook-Air.local>
2023-02-27 00:25:57 +0100

    feat: add bakery algorithm

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
-"""This module contains an implementation of ticket algorithm.
+"""
+This module contains an implementation of the Bakery algorithm.
 
-Ticket algorithm assures mutual exclusion of N threads.
+The Bakery algorithm assures mutual exclusion of N threads.
 Beware! This is an example and may not actually work correctly ;)
 """
 
-__author__ = "Tomáš Vavro"
-__email__ = "xvavro@stuba.sk"
+__author__ = "Tomáš Vavro", "Dominika Bemberáková"
+__email__ = "xvavro@stuba.sk", "xbemberakova@stuba.sk"
 __license__ = "MIT"
 
 from fei.ppds import Thread
 from time import sleep
 
-current_number: int = 0
-next_number: int = 0
+DEFAULT_NUM_RUNS = 10
+NUM_THREADS = 3
+
+
+# array representing the interest of each process in the critical section
+choose = [False] * NUM_THREADS
+# array representing the order in which each process will receive access to the resource
+number = [0] * NUM_THREADS
+
+
+def bakery_algorithm(tid: int):
+    global choose, number
+
+    # assign order to process
+    choose[tid] = True
+    number[tid] = max(number) + 1
+    choose[tid] = False
+
+    # wait for other processes to release the critical section
+    for j in range(NUM_THREADS):
+        # skip current process
+        if j == tid:
+            continue
+        # wait for processes currently deciding on entering the critical section
+        while choose[j]:
+            pass
+        # wait until process with lower number has finished
+        while (number[j] != 0) and ((number[j], j) < (number[tid], tid)):
+            pass
+
+    # critical section
+    # print(f"Thread {tid} is entering the critical section.")
+    sleep(1)
+    print(f"Thread {tid} is leaving the critical section.")
+
+    # release order of process
+    number[tid] = 0
 
 
 def process(tid: int, num_runs: int):
@@ -22,27 +58,10 @@ def process(tid: int, num_runs: int):
         tid      -- thread id
         num_runs -- number of executions of the critical section
     """
-    global current_number, next_number
-
     for _ in range(num_runs):
-        # process wants to enter critical section
-        turn: int = current_number
-        current_number += 1
-
-        # wait for other processes to finish their execution of critical section
-        while turn != next_number:
-            continue
-
-        # execute critical section
-        print(f"Process {tid} runs a complicated computation!")
-        sleep(1)
-
-        # exit critical section
-        next_number += 1
+        bakery_algorithm(tid)
 
 
 if __name__ == '__main__':
-    DEFAULT_NUM_RUNS = 10
-    NUM_THREADS = 3
     threads = [Thread(process, i, DEFAULT_NUM_RUNS) for i in range(NUM_THREADS)]
     [t.join() for t in threads]
